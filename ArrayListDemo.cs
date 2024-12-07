b7c796ced7a3f239b9c322247908ed6e995794c1
SubharajGhosh <ghoshsubharajani34@gmail.com>
2024-12-07 11:18:53 +0530

    Add files via upload

---
+++ ArrayListDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class ArrayListDemo
+    {
+        static void Main(string[] args)
+        {
+            ArrayList cities = new ArrayList();
+            cities.Add("Madrid");
+            cities.Add("Lisbon");
+            cities.Add("Turin");
+            cities.Add("Manchester");
+            cities.Add("118");
+            //cities.AddRange(cities);
+            //foreach (var item in cities)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            ////cities.InsertRange(0, cities);
+            //cities.RemoveRange(3,7);            
+             foreach (var item in cities)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("____________________________");
+            cities.Reverse();
+            foreach (var item in cities)
+            {
+                Console.WriteLine(item);
+            }
+            //Console.WriteLine("Capacity:" + cities.Count);
+        }
+    }
+}
