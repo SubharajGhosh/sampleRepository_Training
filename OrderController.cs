708b25fe784d9578cc1f533c3641d26c483e0830
SubharajGhosh <ghoshsubharajani34@gmail.com>
2024-12-07 10:27:26 +0530

    commit2

---
+++ OrderController.cs
@@ -0,0 +1,51 @@
+﻿using ECommerceApp.Entities;
+using ECommerceApp.Repsoitories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceApp.Controllers
+{
+    public class OrderController : Controller
+    {
+        private OrderRepository orderRepository;
+        public OrderController()
+        {
+            orderRepository = new OrderRepository();
+        }
+        // GET: Order
+        public ActionResult Index()
+        {
+            var orders = orderRepository.GetAll();
+            return View(orders);
+        }
+        public ActionResult Details(string id)
+        {
+            var order = orderRepository.GetOrderById(id);
+            return View(order);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Order order)
+        {
+            orderRepository.AddOrder(order);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            orderRepository.DeleteOrder(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult GetOrders(string id)
+        {
+            var filtered = orderRepository.GetOrdersByProduct(id);
+            return View(filtered);
+        }
+
+    }
+}
---
+++ ProductController.cs
@@ -0,0 +1,57 @@
+﻿using ECommerceApp.Repsoitories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ECommerceApp.Entities;
+
+namespace ECommerceApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ProductRepository productRepository;
+        public ProductController() 
+        { 
+            productRepository = new ProductRepository();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            var products = productRepository.GetAll();
+            return View(products);
+        }
+        public ActionResult Details(string id)
+        {
+            var product = productRepository.Get(id);
+            return View(product);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Add(Product product)
+        {
+            productRepository.Add(product);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(string id) 
+        {
+            var product= productRepository.Get(id);
+            return View(product);
+        }
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            productRepository.Edit(product);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(string id)
+        {
+            productRepository.Delete(id);
+            return RedirectToAction("Index");   
+        }
+       
+    }
+}
